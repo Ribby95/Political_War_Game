f0911df72f0733448d6b86a6e287c005b64964ab
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-01-31 14:48:15 -0500

    main is async but theserver runs forever now

--- netcode/server.py
+++ netcode/server.py
@@ -36,6 +36,8 @@ class Server:
             host=self.host,
             port=self.port
         )
+        self.tasks.add(server_task)
+        await server_task
 
         # give handle_client time to run
         while True:
