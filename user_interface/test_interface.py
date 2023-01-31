d34d3feb31c1416bb6bf74fcd593b3b0de6246f3
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-01-31 14:43:07 -0500

    trying to implement this using pythons asyncio framework

--- user_interface/test_interface.py
+++ user_interface/test_interface.py
@@ -1,23 +1,42 @@
-from netcode.client import Client
+import logging
+import asyncio
 from argparse import  ArgumentParser
 from queue import Queue
-from threading import Thread
+from sys import stdout
 
-def main(host, port: int):
-    username = input("username> ")
-    client = Client(host, port, username)
-    client.join()
-    message_queue = Queue()
-    server_loop = Thread(target=client.loop, args=(message_queue,))
-    server_loop.start()
+from logging import debug, info
+from netcode.client import Client
+from netcode.server import Server
+
+
+async def receive_worker(input: Queue, outfile):
     while True:
-        message = message_queue.get()
-        print(message)
+        debug("waiting on message")
+        print(input.get(block=True),file=outfile)
+        debug("got message")
+
+
+async def send_input(client):
+    message = input(">")
+    info("sending message")
+    client.send(message)
+
+
+async def main(host, port: int):
+    debug("got here")
+    inbox = Queue()
+    client = await Client.new(host, port)
+    async with asyncio.TaskGroup() as task_group:
+        task_group.create_task(client.wait_for_messages(inbox))
+        task_group.create_task(receive_worker(inbox, stdout))
+        task_group.create_task(send_input(client))
+
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     parser = ArgumentParser()
-    parser.add_argument("host")
-    parser.add_argument("port", type=int)
+    parser.add_argument("host", default="localhost")
+    parser.add_argument("port", type=int, default=Server.PORT)
 
     args = parser.parse_args()
-    main(**vars(args))
+    asyncio.run(main(**vars(args)))
