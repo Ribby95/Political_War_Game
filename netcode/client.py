e0434aff9193a95de14ac3e68397c85266723fc8
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-01-31 14:43:07 -0500

    trying to implement this using pythons asyncio framework

---
+++ netcode/client.py
@@ -0,0 +1,36 @@
+import asyncio
+from logging import debug
+from queue import Queue
+import pickle
+
+from netcode import messages
+
+
+class Client:
+
+    @staticmethod
+    async def new(host, port):
+        (reader, writer) = await asyncio.open_connection(host, port)
+        return Client(reader, writer)
+
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+
+    async def send(self, message):
+        debug(f"sending {message!r}")
+        pickle.dump(message, self.writer)
+        await self.writer.drain()
+
+    async def receive(self):
+        debug("receiveing message")
+        message = pickle.load(self.reader)
+        return message
+
+    async def wait_for_messages(self, output: Queue):
+        while True:
+            debug("waiting on message from server")
+            message = await self.receive()
+            debug(f"got message {message}")
+            output.put(message)
+
---
+++ netcode/server.py
@@ -0,0 +1,78 @@
+import asyncio
+import logging
+import random
+
+from logging import info, debug
+from queue import Queue
+from dataclasses import dataclass
+import pickle
+
+
+@dataclass(frozen=False)
+class Session:
+    id: int
+    message_queue: Queue = Queue()
+
+
+class Server:
+    PACKET_SIZE = 2048
+    PORT = 1337
+
+    def __init__(self, host, port=PORT):
+        self.host = host
+        self.port = port
+        self.message_history = []
+        self.sessions = []
+        self.inbox = Queue()
+
+    async def start(self):
+        await asyncio.start_server(
+            client_connected_cb=self.handle_client,
+            host=self.host,
+            port=self.port
+        )
+
+    async def handle_client(self, client_reader, client_writer):
+        new_session = Session(
+            id=random.randint(0, 2 ** 64),  # todo add collision avoidance
+            message_queue=Queue()
+        )
+        self.sessions.append(new_session)  # todo make this not a race-condition waiting to happen
+        async with asyncio.TaskGroup() as user_tasks:
+            user_tasks.create_task(
+                self.get_messages(client_reader, client_id=new_session.id)
+            )
+            user_tasks.create_task(
+                self.send_messages(client_writer, client_queue=new_session.message_queue)
+            )
+
+    async def get_messages(self, client_reader, client_id):
+        while True:
+            message = await pickle.load(client_reader)
+            debug(f"got message {message!r}")
+            message.id = client_id  # todo check if a user tries spoofing ids and spank em
+            self.inbox.put(message)
+            debug("put message in server inbox")
+
+    @staticmethod
+    async def send_messages(client_writer, client_queue):
+        while True:
+            message = await client_queue.get()
+            pickle.dump(message, client_writer)
+
+    async def broadcast(self):
+        while True:
+            message = await self.inbox.get()
+            for session in self.sessions:
+                session.message_queue.put(message)
+
+
+def main():
+    info("started")
+    server = Server(host="localhost", port=1337)
+    asyncio.run(server.start())
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
