03a4d95772d0aa504220845750825cff079cfb41
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-02-01 11:10:21 -0500

    changed encoding to pickle to support more complicated objects

--- netcode/messages.py
+++ netcode/messages.py
@@ -1,19 +1,22 @@
 from logging import debug
 import struct
+import pickle
+
 async def deserialize(reader):
     debug("reading")
     size, = struct.unpack('<L', await reader.readexactly(4))
     debug(f"got message of size {size}")
-    message = await reader.readexactly(size)
+    data = await reader.readexactly(size)
+    message = pickle.loads(data)
     debug(f"got message {message!r}")
     return message
 
 
 async def serialize(writer, message):
-    message = bytes(message)
-    debug(f"writing {message!r} len {len(message)}")
-    writer.write(struct.pack('<L', len(message)))
-    writer.write(message)
+    data = pickle.dumps(message)
+    debug(f"writing {data!r} len {len(data)}")
+    writer.write(struct.pack('<L', len(data)))
+    writer.write(data)
     await writer.drain()
     debug("write done")
 
