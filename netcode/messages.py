9513a4214764841d51939df80da4e7506a378bf5
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-01-31 22:29:14 -0500

    need to encode message as bytes first (I don't know why, the length looks correct)

--- netcode/messages.py
+++ netcode/messages.py
@@ -1,17 +1,19 @@
 from logging import debug
-
+import struct
 async def deserialize(reader):
     debug("reading")
-    message = await reader.reader.readline()
-    debug(f"read message {message!r}")
-    return message.decode()
+    size, = struct.unpack('<L', await reader.readexactly(4))
+    debug(f"got message of size {size}")
+    message = await reader.readexactly(size)
+    debug(f"got message {message!r}")
+    return message
+
 
 async def serialize(writer, message):
-    message += '\n\n\n'
     message = message.encode()
-    debug(f"writing {message!r}")
+    debug(f"writing {message!r} len {len(message)}")
+    writer.write(struct.pack('<L', len(message)))
     writer.write(message)
-    debug("draining")
     await writer.drain()
-    debug("write complete")
+    debug("write done")
 
